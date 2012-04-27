45023364fc52c55ebfc5bfc198f915b5e3fc54d2
Sebastian Edwards <s.thursday@gmail.com>
2012-04-27 17:15:03 +1200

    Adding a middleware to set an access token on a client.

---
+++ lib/cosmos_authentication/middleware/use_token.rb
@@ -0,0 +1,17 @@
+module Cosmos
+  module Middleware
+    class UseToken
+      def initialize(app, key = :current_body)
+        @app = app
+        @key = key
+      end
+
+      def call(env)
+        access_token = env[@key]['access_token']
+        header = {'Authentication' => "Bearer #{access_token}"}
+        env[:client].tap { |client| client.headers.merge!(header) }
+        @app.call env
+      end
+    end
+  end
+end
