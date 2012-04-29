d802af88b1790467ea55734653ee537df223535e
Sebastian Edwards <s.thursday@gmail.com>
2012-04-29 18:42:22 +1200

    Removing warden strategies and related code.

--- lib/cosmos_authentication.rb
+++ lib/cosmos_authentication.rb
@@ -1,12 +1,7 @@
 require "cosmos_authentication/version"
 require "cosmos_authentication/service"
-require "cosmos_authentication/warden" if defined? Warden
 
 module Cosmos
   module Authentication
-    def self.warden_service(service = nil)
-      @warden_service = service unless service.nil?
-      @warden_service || raise('No authentication service configured for warden.'.inspect)
-    end
   end
 end
