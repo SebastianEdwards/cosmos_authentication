feb65ce0d9fef766302218b76adf56011b393056
Sebastian Edwards <s.thursday@gmail.com>
2012-04-29 18:40:49 +1200

    Adding other authentication methods to service class.

--- lib/cosmos_authentication/service.rb
+++ lib/cosmos_authentication/service.rb
@@ -1,31 +1,88 @@
 require "cosmos/service"
+require "cosmos_authentication/middleware/use_token"
 
-module Cosmos
-  module Authentication
-    class Service < Cosmos::Service
-      attr_accessor :client_id, :client_secret
+module CosmosAuthentication
+  class Service < Cosmos::Service
+    attr_accessor :client_id, :client_secret
 
-      def default_env
-        super.merge({
-          data: {
-            client_id:      client_id,
-            client_secret:  client_secret
-          }
-        })
+    def default_env
+      super.merge({
+        data: {
+          client_id:      client_id,
+          client_secret:  client_secret
+        }
+      })
+    end
+
+    def get_token_via_code(code)
+      get_token({
+        client_id:      client_id,
+        client_secret:  client_secret,
+        grant_type:     'code',
+        code:           code
+      })
+    end
+
+    def get_token_via_password(username, password, scope = '')
+      get_token({
+        client_id:      client_id,
+        client_secret:  client_secret,
+        grant_type:     'password',
+        username:       username,
+        password:       password,
+        scope:          scope
+      })
+    end
+
+    def get_token_via_refresh_token(refresh_token, scope = '')
+      get_token({
+        client_id:      client_id,
+        client_secret:  client_secret,
+        grant_type:     'refresh_token',
+        refresh_token:  refresh_token
+      })
+    end
+
+    def providers
+      get_providers
+    end
+
+    def resource_owner(access_token)
+      get_resource_owner(access_token)
+    end
+
+    private
+    def get_token(data)
+      output = call(data: data) do
+        use Cosmos::Middleware::Discover
+        use Cosmos::Middleware::Traverse, 'oauth2-token'
+        use Cosmos::Middleware::Submit
+        use Cosmos::Middleware::Save, :token
       end
 
-      def get_token_via_password(username, password)
-        data
-        output = SERVICE.call(data: data) do
-          use Middleware::Discover
-          use Middleware::Save, :root
-          use Middleware::Traverse, 'oauth2-token'
-          use Middleware::Submit
-          use Middleware::Save, :token
-        end
+      output[:token]
+    end
 
-        output[:token]
+    def get_providers
+      output = call do
+        use Cosmos::Middleware::Discover
+        use Cosmos::Middleware::Traverse, 'providers'
+        use Cosmos::Middleware::Save, :providers
       end
+
+      output[:providers]
+    end
+
+    def get_resource_owner(access_token)
+      output = call(access_token: access_token) do
+        use Middleware::UseToken, :access_token
+        use Cosmos::Middleware::Discover
+        use Cosmos::Middleware::Traverse, 'resource-owners'
+        use Cosmos::Middleware::Traverse, 'current'
+        use Cosmos::Middleware::Save, :resource_owner
+      end
+
+      output[:resource_owner]
     end
   end
 end
